87285ea15547af7b1f1e7c73cc9a7c8ff4cdcb3f
TanvirFahimBD <tanvirfahim6939@gmail.com>
2023-03-02 20:20:12 +0600

    all features done

--- 3.1-simple-todo-react-redux/src/components/Header.js
+++ 3.1-simple-todo-react-redux/src/components/Header.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import notesImg from '../assets/images/notes.png';
 import completeAllImg from '../assets/images/double-tick.png';
 import plusImg from '../assets/images/plus.png';
+import { useDispatch } from 'react-redux';
+import { added, allCompleted, clearCompleted } from '../redux/todos/actions';
 
 const Header = () => {
+    const dispatch = useDispatch()
+    const [inputValue, setInputValue] = useState('');
+    const handleInput = (e) => {
+        e.preventDefault();
+        dispatch(added(inputValue));
+        setInputValue('');
+    };
+
+    const completeHandler = (e) => {
+        dispatch(allCompleted())
+    };
+
+    const clearHandler = (e) => {
+        dispatch(clearCompleted())
+    };
     return (
         <div>
             <form
-                className="flex items-center bg-gray-100 px-4 py-4 rounded-md"
+                className="flex items-center bg-gray-100 px-4 py-4 rounded-md" onSubmit={handleInput}
             >
                 <img
                     src={notesImg}
@@ -18,6 +35,8 @@ const Header = () => {
                     type="text"
                     placeholder="Type your todo"
                     className="w-full text-lg px-4 py-1 border-none outline-none bg-gray-100 text-gray-500"
+                    value={inputValue}
+                    onChange={(e) => setInputValue(e.target.value)}
                 />
                 <button
                     type="submit"
@@ -32,9 +51,9 @@ const Header = () => {
                         src={completeAllImg}
                         alt="Complete"
                     />
-                    <span>Complete All Tasks</span>
+                    <span onClick={completeHandler}>Complete All Tasks</span>
                 </li>
-                <li className="cursor-pointer">Clear completed</li>
+                <li className="cursor-pointer" onClick={clearHandler}>Clear completed</li>
             </ul>
         </div>
     );
